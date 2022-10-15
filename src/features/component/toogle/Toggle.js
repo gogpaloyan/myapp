27dd66a026ac34f6944c39f2812f58d3c71e922a
gogpaloyan <grigorpaloyan96@gmail.com>
2022-10-15 16:11:07 +0400

    b1

--- src/features/component/toogle/Toggle.js
+++ src/features/component/toogle/Toggle.js
@@ -4,10 +4,10 @@ import "./toogle.css"
 function Toogle({value, setValue}){
 
     return (
-    <label class="switch">
+    <div className="switch">
     <input type="checkbox" checked={value} onChange={() => setValue(!value)}/>
-    <span class="slider round"></span>
-  </label>
+    <span className="slider round"></span>
+  </div>
     )
 }
 
